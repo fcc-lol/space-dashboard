bb0db10c73b61e5adf7733c4795902136b494834
Leo Mancini <mancini@leo.gd>
2025-10-12 20:41:35 -0400

    Enable routing

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true
+  },
+  preview: {
+    historyApiFallback: true
+  },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+    outDir: "dist",
+    assetsDir: "assets"
   }
-})
+});
