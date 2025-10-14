b602b73af28bdf4402a23a1b0f02476739c67013
Leo Mancini <mancini@leo.gd>
2025-10-13 20:24:38 -0400

    Making rows flex to fill height

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,10 +3,17 @@ import styled from "styled-components";
 
 const Container = styled.div`
   width: 100%;
+  height: 100%;
   color: inherit;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
 `;
 
 const Row = styled.div`
+  width: calc(100% - 3rem);
+  flex: 1;
   display: flex;
   justify-content: space-between;
   align-items: center;
