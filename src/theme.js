3b9fb8f8f932f361f7002e3329445aec16b24e50
Leo Mancini <mancini@leo.gd>
2025-10-23 20:31:11 -0400

    Updating moon background to be black

--- src/theme.js
+++ src/theme.js
@@ -13,7 +13,7 @@ export const moonTheme = {
   primary: "#7a7a94",
   secondary: "#1c1c2b",
   tertiary: "#11111d",
-  background: "#050510",
+  background: "#000000",
   text: "#e6f0ff",
   secondaryText: "#9999b1"
 };
